91abb38d94ce2b55e0e3fbcd6b2f6542577f8406
Ido Sheffer <Ido.Sheffer@kubemq.io>
2019-07-03 11:46:03 +0300

    Added messagebody class, added msmq_receiver_win , added web site fro FE

---
+++ WindowsServer/messagebody/MessageBody.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace messagebody
+{
+    [Serializable]
+    public class MessageBody
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Ask { get; set; }
+        public string Bid { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID:{ID}, Name:{Name}, Ask:{Ask}, Bid:{Bid}";
+        }
+    }
+}
---
+++ WindowsServer/msmq_reciver_win/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace msmq_reciver_win
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is an framework example soon to be converted to net core, this will replace msmq_receiver project
+            MessageQueue queue = null;
+            try
+            {
+                queue = new MessageQueue(@".\private$\RAQueue", QueueAccessMode.Receive);
+                //queue.SetPermissions("Everyone", MessageQueueAccessRights.FullControl, AccessControlEntryType.Allow);
+                //queue.Authenticate = false;
+                //queue.EncryptionRequired = EncryptionRequired.None;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            var message = queue.Receive();
+            message.Formatter = new BinaryMessageFormatter();
+            Console.WriteLine(message.Body.ToString());
+
+            KubeMQ.SDK.csharp.Events.Channel channel = new KubeMQ.SDK.csharp.Events.Channel(new KubeMQ.SDK.csharp.Events.ChannelParameters
+            {
+                ChannelName = "ratesstore",
+                KubeMQAddress = "192.168.1.189:50000",
+                ClientID = "msmq_reciver",
+                Store = true
+            });
+            channel.SendEvent(new KubeMQ.SDK.csharp.Events.Event
+            {
+                Body = Encoding.UTF8.GetBytes(Newtonsoft.Json.JsonConvert.SerializeObject(message.Body)),
+                Metadata = "Rate message json encoded in UTF8",
+                EventID = message.Id
+            });
+        }
+    }
+}
