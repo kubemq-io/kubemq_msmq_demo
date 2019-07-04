914922c71fb66647fe603c82cb1fe76e8eecd502
Ido Sheffer <Ido.Sheffer@kubemq.io>
2019-07-04 13:47:21 +0300

    added envrieonmnet parameters

--- WindowsServer/msmq_receiver/Program.cs
+++ WindowsServer/msmq_receiver/Program.cs
@@ -9,47 +9,58 @@ namespace msmq_receiver
 {
     class Program
     {
-        const string kubemqAddress = "104.47.142.90:50000";
+        /// <summary>
+        /// this is the KubeMQ message bus address located on Azure
+        /// </summary>
+        private static string ClientID = Environment.GetEnvironmentVariable("CLIENT") ?? $"pub_Demo_{Environment.MachineName}";
+        private static string PubChannel = Environment.GetEnvironmentVariable("PUBCHANNEL") ?? "ratesstore";      
+        private static string CMDChannel = Environment.GetEnvironmentVariable("CMDCHANNEL") ?? "rateCMD";
+        private static string RateMQ = Environment.GetEnvironmentVariable("RateMQ") ?? @".\private$\raqueue";
+        private static string CMDMQ = Environment.GetEnvironmentVariable("RateMQ") ?? @".\private$\receiver";
+
+
         static void Main(string[] args)
         {
-           // this is an framework example soon to be converted to net core, this will replace msmq_receiver project
-            MessageQueue queue = null;
-            try
-            {
-                queue = new MessageQueue(@".\private$\raqueue");
 
-            }
-            catch (Exception ex)
-            {
+            Console.WriteLine("[Demo] Start demo msmq_receiver");
+            //KubeMQ address environment var "KubeMQServerAddress" GRPC port
+            Console.WriteLine($"[Demo] KUBEMQ GRPC address:{Environment.GetEnvironmentVariable("KubeMQServerAddress")}");
+            Console.WriteLine($"[Demo] ClientID:{ClientID}");
+            Console.WriteLine($"[Demo] Publish rates channel PUBCHANNEL:{PubChannel}");
+            Console.WriteLine($"[Demo] Command channel CMDChannel:{CMDChannel}");
 
-            }
 
-            MessageQueue sendqueue = null;
-            try
-            {
-                sendqueue = new MessageQueue(@".\private$\receiver");
-            }
-            catch (Exception ex)
-            {
+            Console.WriteLine($"[Demo] KubeMQ MSMQ SDK message timeout KUBEMQTIMEOUT:{Environment.GetEnvironmentVariable("KUBEMQTIMEOUT")}");
+            Console.WriteLine($"[Demo] KubeMQ MSMQ SDK message channel:{Environment.GetEnvironmentVariable("KUBEMQSCHANNEL")}");
 
-            }
 
+
+            Console.WriteLine($"[Demo] init KubeMQ MessageQueue RateMQ:{RateMQ}");
+            MessageQueue receiveMQ = new MessageQueue(RateMQ);
+            Console.WriteLine($"[Demo] init KubeMQ MessageQueue CMDMQs:{CMDMQ}");
+            MessageQueue sendMQ =new MessageQueue(CMDMQ);
+
+
+
+            //start a task for dequeue messages from MSMSQ using KubeMQ MSMQ SDK
 
             Task msmqsub = Task.Run(() =>
             {
+                /// Init a new sender channel on the KubeMQ to publish recived rates
+                Console.WriteLine($"[Demo][msmqsub] init KubeMQ publish presistance channel  PubChannel:{PubChannel}");
 
                 KubeMQ.SDK.csharp.Events.Channel channel = new KubeMQ.SDK.csharp.Events.Channel(new KubeMQ.SDK.csharp.Events.ChannelParameters
                 {
-                    ChannelName = "ratesstore",
-                    KubeMQAddress = kubemqAddress,
-                    ClientID = "msmq_reciver",
+                    ChannelName = PubChannel,                  
+                    ClientID = ClientID,
                     Store = true
                 });
 
-                queue.ReceiveCompleted += new ReceiveCompletedEventHandler((sender, eventArgs) =>
+                /// KubeMQ msmq message handler
+                receiveMQ.ReceiveCompleted += new ReceiveCompletedEventHandler((sender, eventArgs) =>
                 {
-                    //eventArgs.Message.Formatter = new XmlMessageFormatter(new Type[]
-                    //{typeof(String)});            
+                  
+                    
                     System.IO.Stream stream = new System.IO.MemoryStream(eventArgs.Message.BodyStream);
                     StreamReader reader = new StreamReader(stream);
                     string msgBody = reader.ReadToEnd();
@@ -61,20 +72,27 @@ namespace msmq_receiver
                         Metadata = "Rate message json encoded in UTF8",
                         EventID = eventArgs.Message.Id
                     });
-                    queue.BeginReceive();
+                    receiveMQ.BeginReceive();
                 });
 
-                queue.BeginReceive();
+                sendMQ.BeginReceive();
             });
 
+
+
+
+            //start a task for enqueue comand messages to MSMSQ using KubeMQ MSMQ SDK
             Task msmqcmd = Task.Run(() =>
             {
-                KubeMQ.SDK.csharp.CommandQuery.Responder responder = new KubeMQ.SDK.csharp.CommandQuery.Responder(kubemqAddress);
+                /// Init a new CommandQuery subscriber on the KubeMQ to receive commands
+                Console.WriteLine($"[Demo][msmqcmd] init KubeMQ publish presistance channel  PubChannel:{PubChannel}");
+
+                KubeMQ.SDK.csharp.CommandQuery.Responder responder = new KubeMQ.SDK.csharp.CommandQuery.Responder();
                 responder.SubscribeToRequests(new KubeMQ.SDK.csharp.Subscription.SubscribeRequest()
                 {
                     SubscribeType = KubeMQ.SDK.csharp.Subscription.SubscribeType.Commands,
-                    Channel = "rateCMD",
-                    ClientID = "msmq_reciver"
+                    Channel = CMDChannel,
+                    ClientID = ClientID
 
                 }, (KubeMQ.SDK.csharp.CommandQuery.RequestReceive request) =>
                 {
@@ -82,10 +100,8 @@ namespace msmq_receiver
                     {
                         string strMsg = string.Empty;
                         object body = Encoding.UTF8.GetString(request.Body);
-                        sendqueue.Send(new Message
-                        {
-                    //        Formatter =  new XmlMessageFormatter(new Type[]
-                    //{typeof(String)}),
+                        sendMQ.Send(new Message
+                        {                   
                             Body = body
                         });
 
@@ -95,7 +111,7 @@ namespace msmq_receiver
                         Body = Encoding.UTF8.GetBytes("o.k"),
                         CacheHit = false,
                         Error = "None",
-                        ClientID = "msmq_reciver",
+                        ClientID = ClientID,
                         Executed = true,
                         Metadata = "OK",
                         Timestamp = DateTime.UtcNow,
